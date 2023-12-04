b6cb931f255a66358744348265d51cd478d2f318
Chkana-Yaroslav <141314535+Chkana-Yaroslav@users.noreply.github.com>
2023-12-04 13:36:24 +0200

    Initial commit

---
+++ js/task-3.js
@@ -0,0 +1,15 @@
+function filterArray(numbers, value) {
+  const filterValue = [];
+  for (const filterElem of numbers) {
+    if (filterElem > value) {
+      filterValue.push(filterElem);
+    }
+  }
+  return filterValue;
+}
+
+console.log(filterArray([1, 2, 3, 4, 5], 3)); // [4, 5]
+console.log(filterArray([1, 2, 3, 4, 5], 4)); // [5]
+console.log(filterArray([1, 2, 3, 4, 5], 5)); // []
+console.log(filterArray([12, 24, 8, 41, 76], 38)); // [41, 76]
+console.log(filterArray([12, 24, 8, 41, 76], 20)); // [24, 41, 76]
